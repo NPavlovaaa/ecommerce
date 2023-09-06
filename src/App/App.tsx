3327df8485fadbcbc80dcfc30a331733b59514af
NPavlovaaa <n.pavlova.web@gmail.com>
2023-09-06 20:50:59 +0300

    Setup Setup project

---
+++ src/App/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css'
+import MainPage from "./pages/MainPage";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}>
+            {/*<Route path="/:id" element={<User />} />*/}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
